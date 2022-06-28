d90e17e0b84192385deff85745fe313f6cf59347
tickheart <66043405+TickHeart@users.noreply.github.com>
2022-06-28 22:55:20 +0800

    添加一个工具函数

--- docs/utils/tools.ts
+++ docs/utils/tools.ts
@@ -4,6 +4,7 @@ export const logWarning = (warning: any) => console.warn(warning)
 export const isClient = typeof window !== 'undefined'
 export const isDef = <T = any>(val?: T): val is T => typeof val !== 'undefined'
 const toString = Object.prototype.toString
+export const isBoolean = (val: any): val is boolean => typeof val === 'boolean'
 export const isFunction = <T extends Function>(val: any): val is T => typeof val === 'function'
 export const isNumber = (val: any): val is number => typeof val === 'number'
 export const isString = (val: unknown): val is string => typeof val === 'string'
